39f59a8568fa5fad06f749fff95b524d10949d28
NotAnyMike <mikewoodcockc@gmail.com>
2019-02-18 09:14:00 +0000

    almost done with ex2

--- Exercise2/QLearning/QLearningBase.py
+++ Exercise2/QLearning/QLearningBase.py
@@ -7,34 +7,80 @@ from DiscreteHFO.Agent import Agent
 class QLearningAgent(Agent):
 	def __init__(self, learningRate, discountFactor, epsilon, initVals=0.0):
 		super(QLearningAgent, self).__init__()
-		
+
+		self._lr = learningRate
+		self._discountFactor = DiscountFactor
+		self._epsilon  = epsilon
+		self._epsilon0 = epsilon
+		self._Q = {}
+		self._steps = 0
+		self._episode = 0
+
+		self._s1 = None
+		self._a  = None
+		self._r  = None
+		self._d  = None
+		self._s2 = None
 
 	def learn(self):
-		raise NotImplementedError
+		Q = self._Q[([self._s, self._a])]
+		all_q = [self._Q[([self._s, pos_a])] for pos_a in self.possibleActions]	
+		Q_max = max(all_q)
+		delta = self._lr * (self._r + self._gamma * Q_max - Q)
+		self._Q[([self._s, self._a])] +=  delta
+		return delta
 
 	def act(self):
-		raise NotImplementedError
+		'''
+		Choose an action and return it
+		'''
+		p = self._epsilon #+ (self._epsilon/len(self.possibleActions))
+		case = np.random.binomial(1, p, 1)
+		if case: # Choose greedy action
+			a = max(self._Q, key=self._Q.get)
+		else: # Choose randomly
+			a = np.random.choice(self.possibleActions)
+		return a
 
 	def toStateRepresentation(self, state):
-		raise NotImplementedError
+		return state
 
 	def setState(self, state):
-		raise NotImplementedError
+		if not state in list(zip(*self.Q.keys())[0]):
+			for a in self.possibleActions:
+				self._Q[(state,a)] = 0
+		self._s1 = state
 
 	def setExperience(self, state, action, reward, status, nextState):
-		raise NotImplementedError
+		self._s1 = state
+		self._a  = action
+		self._r  = reward
+		self._d  = status
+		self._s2 = nextState
 
 	def setLearningRate(self, learningRate):
-		raise NotImplementedError
-
+		self._lr = learningRate
+		
 	def setEpsilon(self, epsilon):
-		raise NotImplementedError
+		self._epsilon = epsilon
 
 	def reset(self):
-		raise NotImplementedError
+		self._epsilon = self._epsilon0
+		#self._Q = {}
+		#self._steps = 0
+		#self._episode = 0
+
+		self._s1 = None
+		self._a  = None
+		self._r  = None
+		self._d  = None
+		self._s2 = None
 		
 	def computeHyperparameters(self, numTakenActions, episodeNumber):
-		raise NotImplementedError
+		# TODO
+		self._episode = episodeNumber
+		self._steps = numTakenActions
+		return self._lr, self._epsilon
 
 if __name__ == '__main__':
 	# Initialize connection with the HFO server
