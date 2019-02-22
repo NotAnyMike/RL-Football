b5f378fa3d8c5a88a8e4e19121df2f7f0eaf6bc0
NotAnyMike <mikewoodcockc@gmail.com>
2019-02-22 22:24:45 +0000

    advancing in ex3

--- Exercise3/Networks.py
+++ Exercise3/Networks.py
@@ -11,7 +11,17 @@ import math
 class ValueNetwork(nn.Module):
 	def __init__(self):
 		super(ValueNetwork, self).__init__()
+                hidden_sizes = [50, 50]
+                input_dim = 50
+                self.layers = []
 
+                for size in range(hidden_sizes):
+                        self.layers.add(nn.Linear(input_dim, size))
+                        input_dim = size
+                
 	def forward(self, inputs) :
-		
-		return 
+                for layer in self.layers[:-1]:
+                        inputs = F.relu(layer(inputs))
+                inputs = layers[-1](inputs)
+
+                return  inputs
--- Exercise3/Worker.py
+++ Exercise3/Worker.py
@@ -2,23 +2,87 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import numpy as np
 
 from Networks import ValueNetwork
 from torch.autograd import Variable
 from Environment import HFOEnv
 import random
 
-def train():
+def train(idx, args, value_network, target_value_network, optimizer, lock, counter, port, seed, I_tar, I_async):
+
+        port = port
+        seed = seed
+        hfoEnv = HFOEnv(numTeammates=0, numOpponents=1, port=port, seed=seed)
+        hfoEnv.connectToServer()
+
+        episodeNumber = 0
+        gamma = 0.99
+        epsilon = 0.8
+        t = 0
+        loss_func = nn.MSELoss()
+        state1 = hfoEnv.reset()
+        for epoch in range(1, args.epochs + 1):
+                #train_epoch(epoch, args, model, device, train_loader, optimizer)
+                if np.random.random() >= epsilon:
+                    action = random.randint(0,3)
+                else:
+                    qs = [compute_prediction((state1+[a],value_network) for a in range(4)]
+                    action = np.argmax(qs)
+
+                a1 = hfoEnv.possibleActions[action]
+
+                state2, reward, done, status, info = hfoEnv.step(a1)
+                print(newObservation, reward, done, status, info)
+
+                if done:
+                        episodeNumber += 1
+
+                y = computeTargets(reward, state2, gamma, done, target_value_network):
+
+                predition = compute_prediction(state1+[a1], value_network)
+                loss = loss_func(prediction, y)
+                loss.backwards()
+
+                state1 = state2
+                t += 1
+                with lock:
+                        counter.value += counter.value + 1
+
+                if t % I_async == 0 or done:
+                        # Async update of value_network using gradients
+
+                        with lock:
+                                # Add grads to value_network
+                                value_network._grad = target_value_network.grad
+                                # Take a step
+                                optimizer.step()
+                                # Clean gradients
+                                optimizer.zero_grad()
+                        target_value_network.zero_grad()
+
+                if counter.value % I_tar == 0:
+                        # Update target network
+                        target_value_network.zero_grad()
+                        target_value_network.load_state_dict(value_network.state_dict())
+
+        #torch.manual_seed(args.seed + rank)
+        #optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
 def computeTargets(reward, nextObservation, discountFactor, done, targetNetwork):
+        if done:
+                y = reward
+        else:
+                qs = [compute_prediction(nextObservation+[a],targeNetwork) for a in range(4)]
+                q_max = np.max(qs)
+                y = reward + discountFactor*q_max
+
+        return y
 
 def computePrediction(state, action, valueNetwork):
+        return valueNetwork(state+[action])
 	
 # Function to save parameters of a neural network in pytorch.
 def saveModelNetwork(model, strDirectory):
 	torch.save(model.state_dict(), strDirectory)
-	
-
-
-
 
