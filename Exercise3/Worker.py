82480e8633d6b78ec3a8e7569cb9f49e96c40e59
NotAnyMike <mikewoodcockc@gmail.com>
2019-03-29 07:22:40 +0000

    updating and organizing files

--- Exercise3/Worker.py
+++ Exercise3/Worker.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
-from tensorboard_logger import Logger
+#from tensorboard_logger import Logger
 from hfo import GOAL
 
 from Networks import ValueNetwork
@@ -22,132 +22,119 @@ def train(idx, args, value_network, target_value_network, optimizer, lock, count
 
         if name == None:
                 name = str(time())
-        logger = Logger("tb/" + name, flush_secs=5)
+        #logger = Logger("tb/" + name, flush_secs=5)
 
-        if args.eval:
-                num_episodes = (args.eval_episodes)
-        else:
-                target_value_network.train()
-                num_episodes = (args.episodes, args.eval_episodes)
+        target_value_network.train()
+        num_episodes = args.episodes
 
         gamma = 0.99
         windows = [10, 500]
         goal_buffer = [0]*max(windows)
 
         max_epsilon = 0.99
-        min_epsilon = 0.1
+        min_epsilon = 0.01
         total = 200
         epsilon_fn = lambda current : max_epsilon - current*(max_epsilon - min_epsilon)/total if current < total else min_epsilon
 
         max_lr = 0.9
         min_lr = 0.1
+        total = 600
         lr_fn = lambda current: max_lr - current*(max_lr - min_lr)/total if current < total else min_lr
 
-        for episodes in num_episodes:
 
-                loss_func = nn.MSELoss()
-                t = 0
-                episodeNumber = 0
-                episodeReward = 0
-                episodeSteps  = 0
-                state1 = hfoEnv.reset()
+        loss_func = nn.MSELoss()
+        t = 0
+        episodeNumber = 0
+        episodeReward = 0
+        episodeSteps  = 0
+        state1 = hfoEnv.reset()
+        
+        while episodeNumber <= num_episodes:
+
+                if (counter.value+1) % 1e3 == 0: 
+                    print("##################################################")
+                    print('t:',t)
+                    print('counter:',counter.value)
+                    print("##################################################")
+                if (counter.value+1) % 1e6 == 0:
+                    saveModelNetwork(value_network,"trained_models/params"+str(int((counter.value+1) // 1e6)))
+
+
+                #train_epoch(epoch, args, model, device, train_loader, optimizer)
+                epsilon = epsilon_fn(episodeNumber)
+                lr = lr_fn(episodeNumber)
+
+                if args.eval or np.random.random() >= epsilon:
+                        qs = value_network(state1)
+                        action = torch.argmax(qs)
+                else:
+                        action = random.randint(0,len(hfoEnv.possibleActions)-1)
+
+                a1 = hfoEnv.possibleActions[action]
+
+                state2, reward, done, status, info = hfoEnv.step(a1)
+                episodeReward += reward
                 
-                if args.eval:
-                        print("##################################################")
-                        print("#################### Evaluation ##################")
-                        print("##################################################")
+                y = computeTargets(reward, state2, gamma, done, target_value_network)
 
-                while episodeNumber <= episodes:
+                prediction = computePrediction(state1,action,value_network)
 
-                        if t % 1e6 == 0:
-                            saveModelNetwork(value_network,"trained_models/params"+str(int(t // 1e6)))
+                Y = torch.zeros(4)
+                Y[action] = y
+                Prediction = torch.zeros(4)
+                Prediction[action] = prediction
 
+                loss = loss_func(Y,Prediction)
+                loss.backward()
 
-                        #train_epoch(epoch, args, model, device, train_loader, optimizer)
-                        epsilon = epsilon_fn(episodeNumber)
-                        lr = lr_fn(episodeNumber)
+                state1 = state2
+                t += 1
+                episodeSteps += 1
+                with lock:
+                        counter.value = counter.value + 1
 
-                        if args.eval or np.random.random() >= epsilon:
-                                qs = value_network(state1)
-                                action = torch.argmax(qs)
+                if done:
+
+                        if status == GOAL:
+                                goal_buffer.append(1)
                         else:
-                                action = random.randint(0,len(hfoEnv.possibleActions)-1)
+                                goal_buffer.append(0)
 
-                        a1 = hfoEnv.possibleActions[action]
+                        #logger.log_value('episode/reward',episodeReward, episodeNumber)
+                        #logger.log_value('episode/length',episodeSteps, episodeNumber)
+                        #logger.log_value('hyperparameters/epsilon', epsilon, episodeNumber)
+                        #logger.log_value('hyperparameters/lr', lr, episodeNumber)
+                        #for window in windows:
+                        #        logger.log_value(learning_str + "goals/%i" % window,
+                        #                        np.sum(goal_buffer[-window:]),
+                        #                        episodeNumber)
+                        episodeNumber += 1
+                        episodeReward = 0.0
+                        episodeSteps  = 0
+                        state1 = hfoEnv.reset()
 
-                        state2, reward, done, status, info = hfoEnv.step(a1)
-                        episodeReward += reward
-                        
-                        #print(state2, reward, done, status, info)
-
-                        y = computeTargets(reward, state2, gamma, done, target_value_network)
-
-                        prediction = computePrediction(state1,action,value_network)
-
-                        Y = torch.zeros(4)
-                        Y[action] = y
-                        Prediction = torch.zeros(4)
-                        Prediction[action] = prediction
-
-                        loss = loss_func(Y,Prediction)
-                        loss.backward()
-
-                        state1 = state2
-                        t += 1
-                        episodeSteps += 1
+                if t % I_async == 0 or done or episodeNumber == num_episodes:
+                        # Async update of value_network using gradients
                         with lock:
-                                counter.value = counter.value + 1
+                                # Add grads to value_network
+                                for param, shared_param in zip(
+                                            value_network.parameters(), 
+                                            target_value_network.parameters()):
+                                        shared_param._grad = param.grad
+                                        #value_network._grad = target_value_network.grad
+                                # Take a step
+                                optimizer.step(lr=lr)
+                                # Clean gradients
+                                optimizer.zero_grad()
+                        target_value_network.zero_grad()
 
-                        if done:
+                #if counter.value % I_tar == 0 or episodeNumber == num_episodes:
+                if t % I_tar == 0 or episodeNumber == num_episodes or done:
+                        # Update target network
+                        target_value_network.zero_grad()
+                        target_value_network.load_state_dict(value_network.state_dict())
 
-                                if status == GOAL:
-                                        goal_buffer.append(1)
-                                else:
-                                        goal_buffer.append(0)
-
-                                learning_str = "eval-" if args.eval else ""
-                                logger.log_value('episode/' + learning_str + 'reward',
-                                        episodeReward, episodeNumber)
-                                logger.log_value('hyperparameters/epsilon', epsilon, episodeNumber)
-                                logger.log_value('hyperparameters/lr', lr, episodeNumber)
-                                for window in windows:
-                                        logger.log_value(learning_str + "goals/%i" % window,
-                                                        np.sum(goal_buffer[-window:]),
-                                                        episodeNumber)
-                                episodeNumber += 1
-                                episodeReward = 0.0
-                                episodeSteps  = 0
-                                state1 = hfoEnv.reset()
-
-                        if args.eval == False:
-                                # TODO this will get some errors in the parallel implementation
-                                # because once one worker updates it, the other wont, and 
-                                # it can occur that a worker never gets to update it
-                                if t % I_async == 0 or done or episodeNumber == episodes:
-                                        # Async update of value_network using gradients
-
-                                        with lock:
-                                                # Add grads to value_network
-                                                for param, shared_param in zip(
-                                                            value_network.parameters(), 
-                                                            target_value_network.parameters()):
-                                                        shared_param._grad = param.grad
-                                                        #value_network._grad = target_value_network.grad
-                                                # Take a step
-                                                optimizer.step(lr=lr)
-                                                # Clean gradients
-                                                optimizer.zero_grad()
-                                        target_value_network.zero_grad()
-
-                                if counter.value % I_tar == 0 or episodeNumber == episodes:
-                                        # Update target network
-                                        target_value_network.zero_grad()
-                                        target_value_network.load_state_dict(value_network.state_dict())
-
-                        hfoEnv.reset()
-
-                if args.eval == False:
-                        args.eval = True
+                hfoEnv.reset()
 
         saveModelNetwork(value_network,"trained_models/params_last")
         # Finishing training and showing stats
--- Exercise3/main.py
+++ Exercise3/main.py
@@ -7,8 +7,8 @@ import sys
 
 import torch
 import torch.multiprocessing as mp
-from tensorboard_logger import Logger
-from datetime import datetime
+#from tensorboard_logger import Logger
+#from datetime import datetime
 
 from Worker import train, computeTargets, computePrediction, saveModelNetwork
 from Networks import ValueNetwork
@@ -18,15 +18,15 @@ from SharedAdam import SharedAdam
 parser = argparse.ArgumentParser(description='PyTorch Asynchronous 1-step Q-learning')
 parser.add_argument('--eval_episodes', type=int, default=0, metavar='n',
                     help='number of epochs to evaluate, will always use greedy policy (default: 500)')
-parser.add_argument('--episodes', type=int, default=500, metavar='n',
+parser.add_argument('--episodes', type=int, default=3000, metavar='n',
                     help='number of epochs to train (default: 500)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log_interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--num_processes', type=int, default=2, metavar='N',
+                    help='how many steps to wait before logging training status')
+parser.add_argument('--num_processes', type=int, default=4, metavar='N',
                     help='how many training processes to use (default: 2)')
 parser.add_argument('--eval', type=bool, default=False, help='Whether or not this trial' \
                         'is in evaluation mode. If it true greed policy is always used')
@@ -73,7 +73,7 @@ if __name__ == "__main__" :
         I_async = 5
 
         processes = []
-        name = str(datetime.now()) + "_"
+        name = ""#str(datetime.now()) + "_"
         for idx in range(0, args.num_processes):
 
                 target_value_network = ValueNetwork().to(device)
@@ -90,8 +90,6 @@ if __name__ == "__main__" :
 
         for p in processes:
                 p.join()
-
-        #saveModelNetwork(value_network, "trained_models/"+ name)
 
         print("##################################################")
         print("###################### done ######################")
